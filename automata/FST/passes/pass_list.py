4876ff82b1326521fdadfc710d195c73c2d011bc
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-01-07 19:33:54 +0000

    Bump the pass_list so the sorter is useable

--- automata/FST/passes/pass_list.py
+++ automata/FST/passes/pass_list.py
@@ -2,6 +2,7 @@ import compute_algebras
 import prefix_split
 import splitter
 import print_algebras
+import sort_by_size
 
 # These passes should work every time inputs are passed to them.
 # This module contains a static list of such passes that can be run
@@ -15,3 +16,5 @@ PrintAlgebras = print_algebras.PrintAlgebraPass()
 
 PrefixSplit = prefix_split.PrefixSplitPass()
 Splitter = splitter.SplitterPass()
+
+SortBySize = sort_by_size.SortBySizePass()
